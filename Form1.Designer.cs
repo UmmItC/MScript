c8b0ddf5358a586d9731ccafb2f9d8cdba50a53c
CsoOBJ1 <undress_undone742@dralias.com>
2023-05-21 19:59:38 +0800

    Login Source file

---
+++ Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace CS_SME_MScript
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            checkBox2 = new CheckBox();
+            checkBox1 = new CheckBox();
+            label4 = new Label();
+            button2 = new Button();
+            button1 = new Button();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Checked = true;
+            checkBox2.CheckState = CheckState.Checked;
+            checkBox2.Font = new Font("Microsoft JhengHei", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            checkBox2.ForeColor = Color.MidnightBlue;
+            checkBox2.Location = new Point(32, 194);
+            checkBox2.Margin = new Padding(2);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(121, 20);
+            checkBox2.TabIndex = 39;
+            checkBox2.Text = "Hide/Show Pass";
+            checkBox2.UseVisualStyleBackColor = true;
+            checkBox2.CheckedChanged += checkBox2_CheckedChanged;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("Microsoft JhengHei", 7.8F, FontStyle.Bold, GraphicsUnit.Point);
+            checkBox1.ForeColor = Color.MidnightBlue;
+            checkBox1.Location = new Point(32, 231);
+            checkBox1.Margin = new Padding(2);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(108, 19);
+            checkBox1.TabIndex = 38;
+            checkBox1.Text = "Remember Pass";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Microsoft JhengHei", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.ForeColor = Color.MidnightBlue;
+            label4.Location = new Point(277, 188);
+            label4.Margin = new Padding(2, 0, 2, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(117, 18);
+            label4.TabIndex = 37;
+            label4.Text = "PassWord Error";
+            label4.Visible = false;
+            // 
+            // button2
+            // 
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Microsoft JhengHei", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.ForeColor = Color.MidnightBlue;
+            button2.Location = new Point(364, 220);
+            button2.Margin = new Padding(2);
+            button2.Name = "button2";
+            button2.Size = new Size(85, 49);
+            button2.TabIndex = 36;
+            button2.Text = "Exit";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Microsoft JhengHei", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.MidnightBlue;
+            button1.Location = new Point(218, 220);
+            button1.Margin = new Padding(2);
+            button1.Name = "button1";
+            button1.Size = new Size(85, 49);
+            button1.TabIndex = 35;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Microsoft JhengHei", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox2.ForeColor = Color.MidnightBlue;
+            textBox2.Location = new Point(153, 144);
+            textBox2.Margin = new Padding(2);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(333, 23);
+            textBox2.TabIndex = 34;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Microsoft JhengHei", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox1.ForeColor = Color.MidnightBlue;
+            textBox1.Location = new Point(153, 103);
+            textBox1.Margin = new Padding(2);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(333, 23);
+            textBox1.TabIndex = 33;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft JhengHei", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = Color.MidnightBlue;
+            label3.Location = new Point(29, 144);
+            label3.Margin = new Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(96, 21);
+            label3.TabIndex = 32;
+            label3.Text = "PassWord :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft JhengHei", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.MidnightBlue;
+            label2.Location = new Point(29, 103);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(99, 21);
+            label2.TabIndex = 31;
+            label2.Text = "UserName :";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft JhengHei", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.MidnightBlue;
+            label1.Location = new Point(183, 48);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(132, 24);
+            label1.TabIndex = 30;
+            label1.Text = "Login System";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(514, 308);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(label4);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login System";
+            FormClosing += Form1_FormClosing;
+            FormClosed += Form1_FormClosed;
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private CheckBox checkBox2;
+        private CheckBox checkBox1;
+        private Label label4;
+        private Button button2;
+        private Button button1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿namespace CS_SME_MScript
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox2.UseSystemPasswordChar = true;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("你確定要關閉本程式嗎?", "Close?", MessageBoxButtons.YesNo) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            MessageBox.Show("Bye Bye", "Exit");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "unknown" && textBox2.Text == "unknown")
+            {
+                this.Hide();
+                Main main_ = new Main();
+                main_.Show();
+                label4.Visible = true;
+                label4.Text = "已成功登入 ! !";
+            }
+
+            else
+            {
+                label4.Visible = true;
+                MessageBox.Show("Error", "Error");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                textBox1.Text = "unknown";
+                textBox2.Text = "unknown";
+            }
+
+            else
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                textBox1.Text = "unknown";
+                textBox2.Text = "unknown";
+            }
+
+            else
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+            }
+        }
+    }
+}
